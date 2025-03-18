7e70fea3a690048054b8c26e965b3d90f2b4b2c4
Pedro <148808390+pedromxrtins@users.noreply.github.com>
2025-03-18 20:34:19 -0300

    Update UserController.java

--- src/main/java/com/projetointegrador/api/controller/UserController.java
+++ src/main/java/com/projetointegrador/api/controller/UserController.java
@@ -2,11 +2,13 @@ package com.projetointegrador.api.controller;
 
 import com.projetointegrador.api.model.User;
 import com.projetointegrador.api.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*") 
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -17,8 +19,20 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        try {
+            User newUser = userRepository.save(user);
+            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
